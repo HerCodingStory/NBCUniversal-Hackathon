f52611fd26337cbf908196a2ac34a0e288936861
Cristina <cristyevr94@gmail.com>
2018-11-04 01:33:50 -0400

    front end

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import HomeScreen from './components/HomeScreen';
+import ShowScreen from './components/ShowScreen';
+import ProfileScreen from './components/ProfileScreen';
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Show: ShowScreen,
+    Profile: ProfileScreen,
+  },
+  {
+    initialRouteName: 'Home',
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: 'navy',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  },
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
